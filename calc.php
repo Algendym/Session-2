45fabf66cf456b28a135b27dc833e2a8db1dc517
Algendym <113259059+Algendym@users.noreply.github.com>
2022-09-12 00:23:11 +0200

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calculator</title>
+</head>
+<style>
+    body{
+        background-color: wheat;
+        text-align: center;
+        margin-top: 15%;
+    }
+</style>
+<body>
+
+
+<?php
+
+
+
+    $num1 = 6;
+    $num2 = 3;
+
+    echo "6 + 3 = " . $num1 + $num2;
+    echo "<br>" . "6 x 3 = " . $num1 * $num2;
+    echo "<br>" . "6 / 3 = " . $num1 / $num2;
+    echo "<br>" . "6 - 3 = " . $num1 - $num2;
+
+
+
+
+
+
+?>
+</body>
+</html>
+
+
+
+
